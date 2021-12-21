32bc814831afdb88edc84c5b34180b0522644c1f
krphillips <krphillips@usgs.gov>
2021-12-21 14:58:34 -0500

    initial commit

---
+++ R/EchoNet2Fish-package.R
@@ -0,0 +1,34 @@
+#' Estimate Fish Abundance from Acoustic Echoes and Net Catch
+#'
+#' \pkg{EchoNet2Fish} estimates fish abundance from acoustic echoes and 
+#' midwater trawl catch.
+#'
+#' An example of how to use the functions in \pkg{EchoNet2Fish} is given in this
+#' vignette
+#' \href{https://rawgit.com/krphillips/EchoNet2Fish/vignettes/Intro.html}{[link]}.
+#' Use \code{\link{readAll}} to read in the acoustic and midwater trawl data.
+#' Use \code{\link{exploreACMT}} and \code{\link{estimateLake}} to 
+#'   explore the data and estimate abundance.
+#'
+#' \emph{U.S. Geological Survey} (USGS) Computer Program \pkg{EchoNet2Fish} 
+#'   version 1.0.000
+#' Written by Jean Adams, Dave Warner, and Kristy Phillips from the USGS
+#;     Great Lakes Science Center
+#' \href{http://www.glsc.usgs.gov/}{glsc.usgs.gov}, Ann Arbor, Michigan, USA.
+#' Written in programming language R (R Core Team, 2018, www.R-project.org),
+#' version 1.0.000 (2021-11-19). 
+#'
+#' Source code is available from Kristy Phillips on GitHub:
+#' \href{https://github.com/krphillips/EchoNet2Fish}{github.com/krphillips/EchoNet2Fish},
+#' \emph{krphillips (at) usgs (dot) gov}.
+#'
+#' \emph{Disclaimer:}  Although this program has been used by the USGS,
+#' no warranty, expressed or implied, is made by the USGS or the United States
+#' Government as to the accuracy and functioning of the program and related
+#' program material nor shall the fact of distribution constitute any such
+#' warranty, and no responsibility is assumed by the USGS in connection
+#' therewith.
+#'
+#' @name EchoNet2Fish
+#' @docType package
+NULL
