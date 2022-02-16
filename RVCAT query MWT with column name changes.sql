aaa0738b82e697e6dcf69010de7fa6fabe0a5f22
krphillips <35300105+krphillips@users.noreply.github.com>
2022-02-16 12:19:17 -0500

    sql script to query required trawl files
    This sql script queries RVCAT database for midwater trawl op data, midwater trawl catch data, and midwater trawl length frequency data. The "WHERE" statement after each script needs to be edited to reflect the vessel(s), year, cruise, etc you want included in the query/analysis.

---
+++ RVCAT query MWT with column name changes.sql
@@ -0,0 +1,23 @@
+
+/*mtr.op file for EchoNet2Fish*/   
+SELECT 
+        year "Year", lake "Lake", vessel "Vessel", cruise "Cruise", op.op_id as "Op_Id", op.port as "Port", port_name as "Port_Name", transect as "Transect", target as "Target", 
+        tr_design as "Tr_Design", type_set as "Type_Set", serial as "Serial", fishing_depth as "Fishing_Depth", fishing_temp as "Fishing_Temp", beg_depth as "Beg_Depth", 
+        end_depth as "End_Depth", distance as "Disance", tow_time as "Tow_Time", beg_latitude_dd as "Latitude", beg_longitude_dd as "Longitude"
+FROM    rvcat.op, rvcat.tr_op t, rvcat.op_target g, rvcat.port p
+WHERE   op.op_id=t.op_id and op.op_id=g.op_id(+)and op.port=p.port and lake = 3 and sample_type = 1 and vessel in (88,35) and target in (119, 210) and year = 2021;
+      
+/*Huron.mtr.catch file for EchoNet2Fish*/
+
+SELECT
+         v.year as "Year", v.lake "Lake", v.vessel as "Vessel", v.cruise as "Cruise", v.op_id "Op_Id", op_date "Op_Date",  c.N, v.serial "Serial", v.sample_Type "Sample_Type", 
+         c.species as "Species", c.weight as "Weight", c.life_Stage "Life_Stage"
+FROM     rvcat.op_view v join rvcat.tr_catch c on v.op_id = c.op_id
+WHERE    v.lake = 3 and v.sample_type = 1 and v.vessel IN (88, 35) and v.cruise in (2,6) and v.year = 2021;
+
+/*Huron.mtr.tr_lf file for EchoNet2Fish*/
+SELECT 
+        v.op_id "Op_Id", v.year "Year", v.lake "Lake", v.vessel "Vessel", v.cruise "Cruise", v.port "Port", v.OP_DATE "Op_Date", v.transect "Transect", v.serial "Serial",
+        v.sample_type "Sample_Type", lf.species "Species", lf.length "Length", lf.N "N", lf.life_stage "Life_Stage"
+FROM    rvcat.op_view v JOIN rvcat.tr_lf lf on v.OP_ID = lf.OP_ID
+WHERE   v.year = 2021 and v.lake = 3  and v.vessel in( 35, 88) and v.cruise in (2,6);
