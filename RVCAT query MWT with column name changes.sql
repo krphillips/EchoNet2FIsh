9fa38cb70887a3c770f94da1d934e18bd61a1c60
krphillips <35300105+krphillips@users.noreply.github.com>
2022-02-16 12:38:30 -0500

    Update RVCAT query MWT with column name changes.sql

--- RVCAT query MWT with column name changes.sql
+++ RVCAT query MWT with column name changes.sql
@@ -1,17 +1,17 @@
 
 /*mtr.op file for EchoNet2Fish*/   
 SELECT 
-        year "Year", lake "Lake", vessel "Vessel", cruise "Cruise", op.op_id as "Op_Id", op.port as "Port", port_name as "Port_Name", transect as "Transect", target as "Target", 
-        tr_design as "Tr_Design", type_set as "Type_Set", serial as "Serial", fishing_depth as "Fishing_Depth", fishing_temp as "Fishing_Temp", beg_depth as "Beg_Depth", 
-        end_depth as "End_Depth", distance as "Disance", tow_time as "Tow_Time", beg_latitude_dd as "Latitude", beg_longitude_dd as "Longitude"
+        year "Year", lake "Lake", vessel "Vessel", cruise "Cruise", op.op_id "Op_Id", op.port "Port", port_name "Port_Name", transect "Transect", target "Target", 
+        tr_design "Tr_Design", type_set "Type_Set", serial "Serial", fishing_depth "Fishing_Depth", fishing_temp "Fishing_Temp", beg_depth "Beg_Depth", 
+        end_depth "End_Depth", distance "Disance", tow_time "Tow_Time", beg_latitude_dd "Latitude", beg_longitude_dd "Longitude"
 FROM    rvcat.op, rvcat.tr_op t, rvcat.op_target g, rvcat.port p
 WHERE   op.op_id=t.op_id and op.op_id=g.op_id(+)and op.port=p.port and lake = 3 and sample_type = 1 and vessel in (88,35) and target in (119, 210) and year = 2021;
       
 /*Huron.mtr.catch file for EchoNet2Fish*/
 
 SELECT
-         v.year as "Year", v.lake "Lake", v.vessel as "Vessel", v.cruise as "Cruise", v.op_id "Op_Id", op_date "Op_Date",  c.N, v.serial "Serial", v.sample_Type "Sample_Type", 
-         c.species as "Species", c.weight as "Weight", c.life_Stage "Life_Stage"
+         v.year "Year", v.lake "Lake", v.vessel "Vessel", v.cruise "Cruise", v.op_id "Op_Id", op_date "Op_Date",  c.N, v.serial "Serial", v.sample_Type "Sample_Type", 
+         c.species "Species", c.weight "Weight", c.life_Stage "Life_Stage"
 FROM     rvcat.op_view v join rvcat.tr_catch c on v.op_id = c.op_id
 WHERE    v.lake = 3 and v.sample_type = 1 and v.vessel IN (88, 35) and v.cruise in (2,6) and v.year = 2021;
 
